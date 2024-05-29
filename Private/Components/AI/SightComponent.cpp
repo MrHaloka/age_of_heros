d20b2b6913d50dd45afe3d720290afc082969660
Hamed <lotfikazemihamed@gmail.com>
2024-05-29 12:24:11 +0330

    Add SightComponent. Close #14

---
+++ Private/Components/AI/SightComponent.cpp
@@ -0,0 +1,47 @@
+#include "Components/AI/SightComponent.h"
+
+#include "GamePlay/GameStatics.h"
+#include "GamePlay/RTSGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Managers/ObjectsManager.h"
+#include "Managers/PerceptionManager.h"
+
+USightComponent::USightComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void USightComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	GameStatics::GetObjectManager()->GetPerceptionManager()->RegisterUnitSightPerception(StaticCast<ABaseUnit*>(GetOwner()));
+}
+
+void USightComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (ARTSGameMode* GameMode = Cast<ARTSGameMode>(UGameplayStatics::GetGameMode(GetWorld())))
+	{
+		GameMode->GetObjectManager()->GetPerceptionManager()->RemoveUnitSightPerception(StaticCast<ABaseUnit*>(GetOwner()));
+	}
+	Super::EndPlay(EndPlayReason);
+}
+
+FVector2d USightComponent::GetPerceptionCenter()
+{
+	return StaticCast<ABaseUnit*>(GetOwner())->GetActorLocation2d();
+}
+
+uint32 USightComponent::GetPerceptionRadius()
+{
+	return PerceptionRadius;
+}
+
+void USightComponent::Sensed(const ABaseUnit* Unit)
+{
+	OnSenseUnitEvent.Broadcast(Unit);
+}
+
+uint32 USightComponent::GetPerceptionRadiusSquare()
+{
+	return PerceptionRadius*PerceptionRadius;
+}
---
+++ Public/Components/AI/SightComponent.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Units/BaseUnit.h"
+#include "Components/ActorComponent.h"
+#include "SightComponent.generated.h"
+
+class AMoveableUnit;
+class ABaseUnit;
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class AOE2_API USightComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	USightComponent();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	UPROPERTY(EditDefaultsOnly, Category="Perception|Raduis")
+	int32 PerceptionRadius;
+public:	
+	FVector2d GetPerceptionCenter();
+	uint32 GetPerceptionRadius();
+	uint32 GetPerceptionRadiusSquare();
+	UFUNCTION()
+	void Sensed(const ABaseUnit* Unit);
+public:
+	DECLARE_EVENT_OneParam(USightComponent, FOnSenseUnitEvent, const ABaseUnit*);
+	FOnSenseUnitEvent OnSenseUnitEvent;
+};
