2cd8ecdeadaf1ed6a3388225d4259494322fd7e1
Hamed <lotfikazemihamed@gmail.com>
2024-06-08 14:59:34 +0330

    Add Arrow projectile. Close #13

---
+++ Private/Actors/Projectils/ArrowProjectile.cpp
@@ -0,0 +1,2 @@
+#include "Actors/Projectils/ArrowProjectile.h"
+
---
+++ Public/Actors/Projectils/ArrowProjectile.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseProjectile.h"
+#include "GameFramework/Actor.h"
+#include "ArrowProjectile.generated.h"
+enum class ETeams;
+class ABaseUnit;
+
+UCLASS()
+class AOE2_API AArrowProjectile : public ABaseProjectile
+{
+	GENERATED_BODY()
+};
