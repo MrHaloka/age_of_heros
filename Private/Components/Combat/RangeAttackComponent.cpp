caa5f8d357d15b0c37e916ec3b70664979908224
Hamed <lotfikazemihamed@gmail.com>
2024-06-08 15:03:48 +0330

    Add Range Attack component.

---
+++ Private/Components/Combat/RangeAttackComponent.cpp
@@ -0,0 +1,43 @@
+﻿#include "Components/Combat/RangeAttackComponent.h"
+#include "Actors/Projectils/BaseProjectile.h"
+#include "Actors/Units/MoveableUnit.h"
+#include "Components/AI/SightComponent.h"
+#include "Engine/DamageEvents.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void URangeAttackComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void URangeAttackComponent::AttackCompleted()
+{
+	if (Target == nullptr)
+	{
+		return;
+	}
+	check(GetOwner()->IsA(AMoveableUnit::StaticClass()))
+	StaticCast<AMoveableUnit*>(GetOwner())->Chase(Target, MaxRange);
+}
+
+void URangeAttackComponent::PerformAttack()
+{
+	ABaseProjectile* Arrow = GetWorld()->SpawnActorDeferred<ABaseProjectile>(ProjectileType, GetOwner()->GetTransform(), GetOwner(),GetWorld()->GetFirstPlayerController()->GetPawnOrSpectator(), ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+	if (Arrow)
+	{
+		Arrow->SetOwner(GetOwner());
+		Arrow->SetDamage(Damage);
+		FTransform ArrowRotation = Arrow->InitializeProjectile(StaticCast<ABaseUnit*>(GetOwner())->GetActorCenterLocation2d(), Target->GetActorLocation() + FVector(0,0, Target->GetActorHeight() / 2),  StaticCast<ABaseUnit*>(GetOwner())->GetActorHeight() / 2);
+		UGameplayStatics::FinishSpawningActor(Arrow, ArrowRotation);
+	}
+	if (AMoveableUnit* MoveableOwner = Cast<AMoveableUnit>(GetOwner()))
+	{
+		MoveableOwner->StopChasing(Attacking);
+		FTimerHandle StartChasingTimer;
+		FTimerDelegate StartChasingDelegate;
+		StartChasingDelegate.BindUFunction(this, "AttackCompleted");
+		GetWorld()->GetTimerManager().SetTimer(StartChasingTimer, StartChasingDelegate, AttackLength, false);
+	}
+}
+
---
+++ Public/Components/Combat/RangeAttackComponent.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCombatComponent.h"
+#include "RangeAttackComponent.generated.h"
+
+
+class ABaseProjectile;
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AOE2_API URangeAttackComponent : public UBaseCombatComponent
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	UFUNCTION()
+	void AttackCompleted();
+	virtual void PerformAttack() override;
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Projectile")
+	TSubclassOf<ABaseProjectile> ProjectileType;
+};
