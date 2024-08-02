60d4b46b856f5766f048b15ac7348ca23520faab
Hamed <lotfikazemihamed@gmail.com>
2024-08-02 17:47:35 +0330

    Add NoiseData. Hold noise scale and range.

---
+++ Public/DataStructures/MapGeneration/NoiseData.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+
+struct FNoiseData
+{
+	FNoiseData(float InNoiseScale, float InNoiseRange): NoiseScale(InNoiseScale), NoiseRange(InNoiseRange)
+	{
+		check(NoiseRange >= 0 && NoiseRange <= 2)
+	}
+	FNoiseData():NoiseScale(0), NoiseRange(0)
+	{
+	}
+	float NoiseScale;
+	float NoiseRange;
+};
