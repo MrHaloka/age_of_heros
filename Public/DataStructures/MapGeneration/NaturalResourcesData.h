2d159e004d23d8ab1338d85dcc2f4b6ac077ddde
Hamed <lotfikazemihamed@gmail.com>
2024-08-02 17:47:08 +0330

    Add FNaturalResourcesData. Hold data for natural resources.

---
+++ Public/DataStructures/MapGeneration/NaturalResourcesData.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "ResourceSpawnerData.h"
+
+struct FNaturalResourcesData
+{
+	FNaturalResourcesData(TArray<FResourceSpawnerData>&& InNaturalResources, int32 InForestPartition, int32 InMinForestTiles, int32 InMaxForestTiles):
+		NaturalResources(MoveTemp(InNaturalResources)), ForestPartition(InForestPartition), MinForestTiles(InMinForestTiles), MaxForestTiles(InMaxForestTiles)
+	{
+	}
+	
+	FNaturalResourcesData(): ForestPartition(0), MinForestTiles(0), MaxForestTiles(0)
+	{
+	}
+	
+	TArray<FResourceSpawnerData> NaturalResources;
+	int32 ForestPartition;
+	int32 MinForestTiles;
+	int32 MaxForestTiles;
+
+};
