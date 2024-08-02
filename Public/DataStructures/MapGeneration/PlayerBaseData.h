b2ae7de8e5cdead07b2f97f67e7797071f5822d2
Hamed <lotfikazemihamed@gmail.com>
2024-08-02 17:48:28 +0330

    Add PlayerBaseData. This data is used to spawn players.

---
+++ Public/DataStructures/MapGeneration/PlayerBaseData.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+struct FPlayerBaseData
+{
+	FPlayerBaseData(float InMinPlayersDistance, float InCenterRadius, float InSafeArea = 0):
+		MinPlayersDistance(InMinPlayersDistance),
+		CenterRadius(InCenterRadius),
+		SafeArea(InSafeArea)
+	{
+	}
+	FPlayerBaseData(): MinPlayersDistance(0), CenterRadius(0), SafeArea(0)
+	{
+	}
+
+	float MinPlayersDistance;
+	float CenterRadius;
+	float SafeArea;
+};
