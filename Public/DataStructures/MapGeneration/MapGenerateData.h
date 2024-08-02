7519dd5de86e04d25dfb5325932951568f23de52
Hamed <lotfikazemihamed@gmail.com>
2024-08-02 17:46:05 +0330

    Add MapGeneratorData structure. Hold all necessary data to generate a map.

---
+++ Public/DataStructures/MapGeneration/MapGenerateData.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "NaturalResourcesData.h"
+#include "NoiseData.h"
+#include "PlayerBaseData.h"
+
+struct FMapGenerateData
+{
+	FMapGenerateData(
+		TArray<FNoiseData>&& InNoiseData,
+		FNoiseData&& InSecondLayer,
+		TArray<FResourceSpawnerData>&& InPlayersResourceData,
+		FNaturalResourcesData&& InNaturalResourcesData,
+		FPlayerBaseData&& InPlayerBaseData
+	):
+		NoiseData(MoveTemp(InNoiseData)),
+		SecondLayer(MoveTemp(InSecondLayer)),
+		PlayersResourceData(MoveTemp(InPlayersResourceData)),
+		NaturalResourcesData(MoveTemp(InNaturalResourcesData)),
+		PlayerBaseData(MoveTemp(InPlayerBaseData))
+	{
+	}
+
+	FMapGenerateData()
+	{
+	}
+
+	TArray<FNoiseData> NoiseData;
+	FNoiseData SecondLayer;
+	TArray<FResourceSpawnerData> PlayersResourceData;
+	FNaturalResourcesData NaturalResourcesData;
+	FPlayerBaseData PlayerBaseData;
+};
